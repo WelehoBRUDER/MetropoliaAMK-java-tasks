18cb78a9fbcdf9965a7fdde224404e88228911a1
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-01-13 14:30:18 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void print(String args) {
+        System.out.println(args);
+    }
+    public static void main(String[] args) {
+        print("Hello World");
+    }
+}
