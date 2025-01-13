ea0c57ee6fed709a57ec87dd357407d06a762f24
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-01-13 14:49:58 +0200

    some tasks

---
+++ src/AskInts.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AskInts {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The product of the numbers is " + (first * second * third));
+        float average = (float) (first + second + third) / 3;
+        System.out.println("The average of the numbers is " + average);
+    }
+}
