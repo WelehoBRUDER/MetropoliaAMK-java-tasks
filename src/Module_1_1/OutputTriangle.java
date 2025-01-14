c3c708c0ee66b082ce91f18a6c1b5ba3407a476a
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-01-14 13:10:24 +0200

    for fun

--- src/Module_1_1/OutputTriangle.java
+++ src/Module_1_1/OutputTriangle.java
@@ -1,8 +1,13 @@
+package Module_1_1;
+
 public class OutputTriangle {
     public static void main(String[] args) {
-        System.out.println("    *");
-        System.out.println("   ***");
-        System.out.println("  *****");
-        System.out.println(" *******");
+        final int rows = 4;
+        final int maxItems = rows * 2 + 1;
+        for (int i = 0; i < rows; i++) {
+            int spaces = maxItems / 2 - i;
+            int stars = i * 2 + 1;
+            System.out.println(" ".repeat(spaces) + "*".repeat(stars));
+        }
     }
 }
