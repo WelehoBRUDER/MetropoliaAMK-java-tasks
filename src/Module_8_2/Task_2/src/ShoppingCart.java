f8c00c74917fce43c620580f8d78c19722e9eeb6
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-03-11 11:13:22 +0200

    shoppingcart done

--- src/Module_8_2/Task_2/src/ShoppingCart.java
+++ src/Module_8_2/Task_2/src/ShoppingCart.java
@@ -1,13 +1,26 @@
-class ShoppingCart {
-    public void addItem(String item, double price) {}
+import java.util.ArrayList;
 
-    public void removeItem(String item) {}
+class ShoppingCart {
+    private ArrayList<Item> cart = new ArrayList<>();
+
+    public void addItem(String item, double price) {
+        this.cart.add(new Item(item, price));
+    }
+
+    public void removeItem(String item) {
+        for (Item i : this.cart) {
+            if (i.getName().equals(item)) {
+                this.cart.remove(i);
+                break;
+            }
+        }
+    }
 
     public int getItemCount() {
-        return 0;
+        return this.cart.size();
     }
 
     public double calculateTotal() {
-        return 0.0;
+        return this.cart.stream().mapToDouble(Item::getPrice).sum();
     }
 }
