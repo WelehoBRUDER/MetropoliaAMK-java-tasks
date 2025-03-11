c387f99b65e3c765b6f41a8c0427765432914ca9
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-03-11 11:26:27 +0200

    module 8_3 task 1

---
+++ src/Module_8_3/Task_1/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Person> people = new ArrayList<Person>();
+        people.add(new Person("Markus", 25, "Helsinki"));
+        people.add(new Person("Thomas", 19, "London"));
+        people.add(new Person("Tarmo", 56, "Tampere"));
+        people.add(new Person("Maria", 33, "Bucharest"));
+        people.add(new Person("Laura", 42, "London"));
+
+        // Sort the people list by age (ascending)
+        people.sort(Comparator.comparingInt(Person::getAge));
+
+        System.out.println("People sorted by age:");
+        // Print the sorted list
+        for (Person p : people) {
+            System.out.println(p.getName() + " " + p.getAge() + " " + p.getCity());
+        }
+
+        // Filter to only london
+        people = people.stream().filter(p -> p.getCity().equals("London")).collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println("\nOnly people from London:");
+        // Print the filtered list
+        for (Person p : people) {
+            System.out.println(p.getName() + " " + p.getAge() + " " + p.getCity());
+        }
+    }
+}
