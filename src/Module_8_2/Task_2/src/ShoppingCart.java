4e64a37ddf6cdf7cad344612af52c6c4d8a68862
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-03-11 10:31:30 +0200

    module 8_2 halfway done

---
+++ src/Module_8_2/Task_2/src/ShoppingCart.java
@@ -0,0 +1,13 @@
+class ShoppingCart {
+    public void addItem(String item, double price) {}
+
+    public void removeItem(String item) {}
+
+    public int getItemCount() {
+        return 0;
+    }
+
+    public double calculateTotal() {
+        return 0.0;
+    }
+}
